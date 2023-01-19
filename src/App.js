0e4553e900e3802fcb1cfc06b5437d824cd79ed9
GooseCampagne <gustavodante202@gmail.com>
2023-01-19 11:40:23 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Button = styled.button`
+/* Adapt the colors based on primary prop */
+background: ${props => props.primary ? "palevioletred" : "white"};
+color: ${props => props.primary ? "white" : "palevioletred"};
+
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+// A new component based on Button, but with some override styles
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Button as="a" href="https://twitter.com/ElvisTe78827257">Mi twitter</Button>
+    <Button as="a" href="https://utd.edu.mx/">UTD</Button>
+    <Button as="a" href="https://github.com/GooseCampagne">github</Button>
+   
     </div>
   );
 }
